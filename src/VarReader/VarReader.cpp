78e658257369bb5e56e83a73dcc47772dba83117
Piotr Wasilewski <piwasilewski@interia.pl>
2023-04-30 21:44:47 +0200

    wip: unknown types will be displyed as uint32 to temporarily allow for displaying enum varaibles

--- src/VarReader/VarReader.cpp
+++ src/VarReader/VarReader.cpp
@@ -90,6 +90,8 @@ float VarReader::getFloat(uint32_t address, Variable::type type)
 		return (float)*(int32_t*)&value;
 	else if (type == Variable::type::F32)
 		return *(float*)&value;
+	else if (type == Variable::type::UNKNOWN)
+		return (float)*(uint32_t*)&value;
 
 	return 0.0f;
 }
