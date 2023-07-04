66fc94328ee961ffe1ec59244cdbd4d103702c37
Piotr Wasilewski <piwasilewski@interia.pl>
2023-07-04 18:36:50 +0200

    fix: 0 in sample period means a sequential loop

--- src/Gui/Gui.cpp
+++ src/Gui/Gui.cpp
@@ -500,7 +500,7 @@ void Gui::drawPlotCurveBar(Plot* plot, ScrollingBuffer<double>& time, std::map<s
 			{
 				ImPlot::SetupAxis(ImAxis_Y1, NULL, ImPlotAxisFlags_AutoFit);
 				ImPlot::SetupAxis(ImAxis_X1, "time[s]", 0);
-				const double viewportWidth = settings.samplePeriod * 0.001f * settings.maxViewportPoints;
+				const double viewportWidth = (settings.samplePeriod > 0 ? settings.samplePeriod : 1) * 0.001f * settings.maxViewportPoints;
 				const double min = *time.getLastElement() < viewportWidth ? 0.0f : *time.getLastElement() - viewportWidth;
 				const double max = min == 0.0f ? *time.getLastElement() : min + viewportWidth;
 				ImPlot::SetupAxisLimits(ImAxis_X1, min, max, ImPlotCond_Always);
