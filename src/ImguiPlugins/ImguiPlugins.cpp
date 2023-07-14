d16307a347fb07adbb7f4927eaccb63ada3ee5ad
Piotr Wasilewski <piwasilewski@interia.pl>
2023-07-14 21:51:52 +0200

    style: removed commented block of unused code

--- src/ImguiPlugins/ImguiPlugins.cpp
+++ src/ImguiPlugins/ImguiPlugins.cpp
@@ -1,32 +1,5 @@
 #include "ImguiPlugins.hpp"
 
-// struct InputTextCallback_UserData
-// {
-// 	std::string* Str;
-// 	ImGuiInputTextCallback ChainCallback;
-// 	void* ChainCallbackUserData;
-// };
-
-// static int InputTextCallback(ImGuiInputTextCallbackData* data)
-// {
-// 	InputTextCallback_UserData* user_data = (InputTextCallback_UserData*)data->UserData;
-// 	if (data->EventFlag == ImGuiInputTextFlags_CallbackResize)
-// 	{
-// 		// Resize string callback
-// 		// If for some reason we refuse the new length (BufTextLen) and/or capacity (BufSize) we need to set them back to what we want.
-// 		std::string* str = user_data->Str;
-// 		IM_ASSERT(data->Buf == str->c_str());
-// 		str->resize(data->BufTextLen);
-// 		data->Buf = (char*)str->c_str();
-// 	}
-// 	else if (user_data->ChainCallback)
-// 	{
-// 		// Forward to user callback, if any
-// 		data->UserData = user_data->ChainCallbackUserData;
-// 		return user_data->ChainCallback(data);
-// 	}
-// 	return 0;
-// }
 namespace ImGui
 {
 bool InputText(const char* label, std::string* str, ImGuiInputTextFlags flags)
