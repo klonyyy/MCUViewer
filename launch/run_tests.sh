5ace3165220447965fe7e1befdd0fde96e6109cf
Piotr Wasilewski <piwasilewski@interia.pl>
2023-12-24 10:36:17 +0100

    fix: changes in the test launch file

--- launch/run_tests.sh
+++ launch/run_tests.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 git config --global --add safe.directory /__w/STMViewer/STMViewer
 
-(cd ..; rm -rf build_test)
-(cd ..; mkdir build_test && cd build_test && cmake .. -DMAKE_TESTS=1 && make -j)
-(cd ../build_test/test; ./STMViewer_test)
+rm -rf build_test
+mkdir build_test 
+cd build_test
+cmake .. -DMAKE_TESTS=1
+make -j
+cd ../build_test/test 
+./STMViewer_test
 
