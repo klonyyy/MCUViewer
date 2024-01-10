6c98eedca535307b1ab9148f53cb07d0d392e086
Piotr Wasilewski <piwasilewski@interia.pl>
2024-01-10 22:13:34 +0100

    wip: working on GDB parser for selecting variables

---
+++ src/GdbParser/GdbParser.hpp
@@ -0,0 +1,19 @@
+#ifndef _GDBPARSER_HPP
+#define _GDBPARSER_HPP
+
+class GdbParser
+{
+    public:
+        void parse()
+        {
+            
+        }
+
+    private:
+        std::string elfPath; 
+        std::vector<std::string, uint32_t, std::string> parsedData; 
+
+};
+
+
+#endif
