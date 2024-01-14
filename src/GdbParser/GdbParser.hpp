93da6838c9465358c187c227d68a8a0ead58c7c7
Piotr Wasilewski <piwasilewski@interia.pl>
2024-01-14 22:07:44 +0100

    wip: working on gdb parser for variables

--- src/GdbParser/GdbParser.hpp
+++ src/GdbParser/GdbParser.hpp
@@ -11,6 +11,7 @@
 #include <vector>
 
 #include "ProcessHandler.hpp"
+#include "Variable.hpp"
 
 #ifdef _WIN32
 using CurrentPlatform = WindowsProcessHandler;
@@ -63,14 +64,12 @@ class GdbParser
 			{
 				end = end1;
 				auto variableChunk = out.substr(start, end - start);
-				std::cout << "File found! " << filename << std::endl;
-				std::cout << "variable chunk: " << variableChunk << std::endl;
+				// std::cout << "File found! " << filename << std::endl;
+				// std::cout << "variable chunk: " << variableChunk << std::endl;
 				parseVariableChunk(variableChunk);
 			}
 			start = end;
 		}
-
-		checkVariableType();
 
 		for (auto& [name, address, path, trivial] : parsedData)
 		{
@@ -95,29 +94,82 @@ class GdbParser
 				break;
 
 			std::string variableName = chunk.substr(spacePos + 1, semicolonPos - spacePos - 1);
-			parsedData.push_back({variableName, 0, ""});
+
+			checkVariableType(variableName);
 			start = semicolonPos + 1;
 		}
 	}
 
-	void checkVariableType()
+	void parseUntiltrivial()
 	{
-		for (auto& [name, address, path, trivial] : parsedData)
+	}
+
+	void checkVariableType(std::string& name)
+	{
+		auto out = process.executeCmd(std::string("ptype ") + name + std::string("\n"));
+		auto start = out.find("=");
+		auto end = out.find("\\n", start);
+
+		auto line = out.substr(start + 2, end - start - 2);
+
+		/* remove const and volatile */
+		if (line.find("volatile ", 0) != std::string::npos)
+			line.erase(0, 9);
+		if (line.find("const ", 0) != std::string::npos)
+			line.erase(0, 6);
+		if (line.find("static const ", 0) != std::string::npos)
+			line.erase(0, 13);
+
+		bool isTrivial = checkTrivial(line);
+
+		if (isTrivial)
+			parsedData.push_back({name, 0, "", isTrivial});
+		else
 		{
-			auto out = process.executeCmd(std::string("ptype ") + name + std::string("\n"));
-			auto start = out.find("=");
-			auto end = out.find("\\n", start);
+			auto subStart = 0;
 
-			auto line = out.substr(start + 2, end - start - 2);
+			while (1)
+			{
+				auto semicolonPos = out.find(';', subStart);
+				if (semicolonPos == std::string::npos)
+					break;
 
-			trivial = isTrivial(line);
+				if (out[semicolonPos - 1] == ')')
+				{
+					subStart = subStart = semicolonPos + 1;
+					continue;
+				}
+
+				auto spacePos = out.rfind(' ', semicolonPos);
+
+				if (spacePos == std::string::npos)
+					break;
+
+				auto varName = out.substr(spacePos + 1, semicolonPos - spacePos - 1);
+
+				if (varName == "const")
+				{
+					subStart = subStart = semicolonPos + 1;
+					continue;
+				}
+
+				if (varName[0] == '*')
+					varName.erase(0);
+
+				auto fullName = name + "." + varName;
+
+				checkVariableType(fullName);
+				subStart = semicolonPos + 1;
+			}
 		}
 	}
 
-	bool isTrivial(std::string& line)
+	bool checkTrivial(std::string& line)
 	{
+		if (isTrivial.contains(line))
+			return true;
 		std::cout << line << std::endl;
-		return true;
+		return false;
 	}
 
 	std::vector<VariableData> getParsedData()
@@ -128,6 +180,38 @@ class GdbParser
    private:
 	std::vector<VariableData> parsedData;
 	ProcessHandler<CurrentPlatform> process;
+
+	std::unordered_map<std::string, Variable::type> isTrivial = {
+		{"bool", Variable::type::U8},
+		{"unsigned char", Variable::type::U8},
+
+		{"char", Variable::type::I8},
+		{"signed char", Variable::type::I8},
+
+		{"unsigned short", Variable::type::U16},
+		{"unsigned short int", Variable::type::U16},
+		{"short unsigned int", Variable::type::U16},
+
+		{"short", Variable::type::I16},
+		{"short int", Variable::type::I16},
+		{"signed short", Variable::type::I16},
+		{"signed short int", Variable::type::I16},
+		{"short signed int", Variable::type::I16},
+
+		{"unsigned int", Variable::type::U32},
+		{"unsigned long", Variable::type::U32},
+		{"unsigned long int", Variable::type::U32},
+		{"long unsigned int", Variable::type::U32},
+
+		{"int", Variable::type::I32},
+		{"long", Variable::type::I32},
+		{"long int", Variable::type::I32},
+		{"signed long", Variable::type::I32},
+		{"signed long int", Variable::type::I32},
+		{"long signed int", Variable::type::I32},
+
+		{"float", Variable::type::F32},
+	};
 };
 
 #endif
