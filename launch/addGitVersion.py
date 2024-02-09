97e3e43ec9f7fb0652b8392b87f563185a23517a
Piotr Wasilewski <piwasilewski@interia.pl>
2024-02-09 21:46:18 +0100

    refactor: changed addGitVersion scripts to python

---
+++ launch/addGitVersion.py
@@ -0,0 +1,17 @@
+import subprocess
+
+# Run git log command to get the hash and format it into a Python file
+git_hash = subprocess.check_output(
+    [
+        "git",
+        "log",
+        '--pretty=format:#define GIT_INFO_PRESENT%n static constexpr const char* GIT_HASH = "%H";',
+        "-n",
+        "1",
+    ]
+).decode()
+
+# Write the output to a Python file, creating it if it doesn't exist
+file_path = "../src/gitversion.h"
+with open(file_path, "w+") as file:
+    file.write(git_hash)
---
+++ src/gitversion.h
@@ -0,0 +1,2 @@
+#define GIT_INFO_PRESENT
+ static constexpr const char* GIT_HASH = "ba807fabcfb6ab85323738e0a6ed465850f56970";
