4db4e2dde9883e5be52a328d988b3d601988f353
Piotr Wasilewski <piwasilewski@interia.pl>
2025-01-04 12:29:17 +0100

    fix: increased refresh from elf dealy due to update problems

--- src/Gui/GuiVarTable.hpp
+++ src/Gui/GuiVarTable.hpp
@@ -197,7 +197,7 @@ class VariableTableWindow
 			{
 				performVariablesUpdate = true;
 				/* TODO: examine why elf is not ready to be parsed without the delay */
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
 			}
 
 			shouldPopStyle = true;
